b553005d195b77403769ea896fd4a9a95dfd00c0
Angel Yarmas <angelmoskera@gmail.com>
2015-09-14 16:00:34 -0500

    Modify to add new id for metaboxes

--- templates/archive-photo-gallery.php
+++ templates/archive-photo-gallery.php
@@ -1,16 +1,19 @@
 <?php
+
+//Add Custom Loop
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'pgsa_custom_archive_loop' );
 
 function pgsa_custom_archive_loop() { ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php
-    $age = get_post_meta( get_the_ID(), 'age', true );
-    $gender = get_post_meta( get_the_ID(), 'gender_select', true );
-    $ethnic = get_post_meta( get_the_ID(), 'ethnicity_select', true );
-    $height = get_post_meta( get_the_ID(), '_pgsa_info_height', true );
-    $weight = get_post_meta( get_the_ID(), '_pgsa_info_weight', true );
-    $patient_photos = get_post_meta( get_the_ID(), '_pgsa_photos_demo', true );
+    $patient_photos = get_post_meta( get_the_ID(), '_pgsa_photos_patient-photos', true );
+		    $age 	= get_post_meta( get_the_ID(), '_pgsa_info_age', true );
+		    $gender = get_post_meta( get_the_ID(), '_pgsa_info_gender_select', true );
+		    $ethnic = get_post_meta( get_the_ID(), '_pgsa_info_ethnicity_select', true );
+		    $height = get_post_meta( get_the_ID(), '_pgsa_info_height', true );
+		    $weight = get_post_meta( get_the_ID(), '_pgsa_info_weight', true );
+	    	$detail = get_post_meta( get_the_ID(), '_pgsa_info_procedure_detail', true );
 
 	// Echo the metadata
 	
@@ -21,10 +24,10 @@ function pgsa_custom_archive_loop() { ?>
 		<?php foreach ( $patient_photos as $index => $value ) { ?>
 			<?php if ($index == 0) { ?>
 			<div class="one-fourth first">							
-				<a href="<?php the_permalink() ?>"><img src="<?php echo $value['before_photo'] ?>" /></a>
+				<a href="<?php the_permalink() ?>"><img src="<?php echo $value['_pgsa_photos_before_photo']; ?>" /></a>
 			</div>
 			<div class="one-fourth">								
-				<a href="<?php the_permalink() ?>"><img src="<?php echo $value['after_photo'] ?>" /></a>
+				<a href="<?php the_permalink() ?>"><img src="<?php echo $value['_pgsa_photos_after_photo']; ?>" /></a>
 			</div>
 			<?php } ?>
 		<?php } ?>
