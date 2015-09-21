500b9653a4d317422e72b040315baeca4468671e
Andree Sabas <sm.andree@hotmail.com>
2015-09-21 11:39:17 -0500

    schema markup and single patient template
    - Added schema markup
    - Added new template for patient detail

--- templates/single-photo-gallery.php
+++ templates/single-photo-gallery.php
@@ -8,11 +8,11 @@ add_action( 'genesis_after_entry_content', 'genesis_prev_next_post_nav', 5 );
 function pgsa_prev_next_post_nav() {
 	if ( 'photo-gallery' == get_post_type()) {
 		echo '<div class="wrap"><div class="prev-next-navigation">';
-			previous_post_link( '<div class="previous">%link</div>', '« %title', TRUE, ' ', 'procedures' );	next_post_link( '<div class="next">%link</div>', '%title »', TRUE, ' ', 'procedures' );
+			previous_post_link( '<div class="previous">%link</div>', '<i class="fa fa-chevron-circle-left"></i> %title', TRUE, ' ', 'procedures' );	next_post_link( '<div class="next">%link</div>', '%title <i class="fa fa-chevron-circle-right"></i>', TRUE, ' ', 'procedures' );
 		echo '</div></div>';
 	}
 }
-add_action( 'genesis_after_header', 'pgsa_prev_next_post_nav' );
+add_action( 'genesis_before_content', 'pgsa_prev_next_post_nav' );
 
 //Replace the custom genesis loop
 remove_action( 'genesis_loop', 'genesis_do_loop' );
@@ -32,47 +32,49 @@ function pgsa_custom_single_loop() { ?>
 	// Echo the metadata
 	?>
 
-	<article <?php post_class() ?> >
-
-		<h1><?php the_title(); ?></h1>
-		<?php foreach ( $patient_photos as $value ) { ?>
-
-			<div class="one-half first">							
-				<a href="<?php echo $value['_pgsa_photos_before_photo']; ?>" data-lity><img id="imageresource" src="<?php echo $value['_pgsa_photos_before_photo']; ?>" /></a>
-				<p><span class="pgsa-photo-caption pgsa-photo-caption-before">Before</span><?php if (!empty($value['_pgsa_photos_before_caption'])) { ?>: <?php echo $value['_pgsa_photos_before_caption']; ?><?php } ?></p>
-			</div>
-			<div class="one-half">
-				<a href="<?php echo $value['_pgsa_photos_after_photo']; ?>" data-lity><img id="imageresource" src="<?php echo $value['_pgsa_photos_after_photo']; ?>" /></a>
-				<p><span class="pgsa-photo-caption pgsa-photo-caption-after">After</span><?php if (!empty($value['_pgsa_photos_after_photo'])) { ?>: <?php echo $value['_pgsa_photos_after_caption']; ?><?php } ?></p>
-			</div>
-		<?php } ?>
-
-
-		<section class="pgsa-patient-info">
+	<article itemscope itemtype="http://schema.org/MedicalProcedure" <?php post_class() ?> >
+		<h1><span itemprop="name"><?php echo $procedures ?></span>	<?php the_title(); ?></h1>
+		<section class="pgsa-surgeon-info">
+			<h3>Surgeon: <span> M.D., F.A.C.S.</span></h3>
+			<h3>Location: <span> </span></h3>
+		</section>
+		<section itemscope itemtype="http://schema.org/Person" class="pgsa-patient-info">
  			<div class="one-third first">
 	 			<ul class="pgsa-info-label-list">
-	 				<li><span class="pgsa-info-label-single pgsa-info-label-procedures">Procedures:</span> <?php echo $procedures ?></li>
 	 				<?php if (!empty($age)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-age">Age:</span> <?php echo esc_html( $age ); ?></li><?php } ?>
+	 				<?php if (!empty($gender)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-gender">Gender:</span><span itemprop="gender"> <?php echo esc_html( $gender ); ?></span></li><?php } ?>
 	 			</ul>
  			</div>
  			<div class="one-third">
 	 			<ul class="pgsa-info-label-list">
-	 				<?php if (!empty($ethnic)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-ethnicity">Ethnicity:</span> <?php echo esc_html( $ethnic ); ?></li><?php } ?>
-	 				<?php if (!empty($height)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-height">Height:</span> <?php echo esc_html( $height ); ?></li><?php } ?>
+	 				<?php if (!empty($height)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-height">Height:</span> <span itemprop="height"><?php echo esc_html( $height ); ?></span></li><?php } ?>	
+	 				<?php if (!empty($weight)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-weight">Weight:</span> <span itemprop="weight"><?php echo esc_html( $weight ); ?></span></li><?php } ?>
 	 			</ul>
  			</div>
  			<div class="one-third">
 	 			<ul class="pgsa-info-label-list">
-	 			 	<?php if (!empty($gender)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-gender">Gender:</span> <?php echo esc_html( $gender ); ?></li><?php } ?>
-	 				<?php if (!empty($weight)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-weight">Weight:</span> <?php echo esc_html( $weight ); ?> </li><?php } ?>
+	 			 	<?php if (!empty($ethnic)) { ?><li><span class="pgsa-info-label-single pgsa-info-label-ethnicity">Ethnicity:</span> <?php echo esc_html( $ethnic ); ?></li><?php } ?>
 	 			</ul>
  			</div>
  			<div class="clearfix"></div>
  			<?php if (!empty($detail)) { ?><span class="pgsa-info-label-single pgsa-info-label-detail">Description:</span>
-			<p><?php echo esc_html( $detail ); ?></p><?php } ?>
+			<p itemprop="description"><?php echo esc_html( $detail ); ?></p><?php } ?>
 		</section>
-
-
+		<section>
+		<?php foreach ( $patient_photos as $value ) { ?>
+			<div itemscope itemtype="https://schema.org/ImageGallery">
+				<h3 itemprop="name">Front</h3>
+				<div class="one-half first">							
+					<a href="<?php echo $value['_pgsa_photos_before_photo']; ?>" data-lity><img itemprop="image" class="aligncenter imageresource" src="<?php echo $value['_pgsa_photos_before_photo']; ?>" /></a>
+					<p class="pgsa-photo-caption"><span class="pgsa-photo-caption-before">Before</span><?php if (!empty($value['_pgsa_photos_before_caption'])) { ?>: <?php echo $value['_pgsa_photos_before_caption']; ?><?php } ?></p>
+				</div>
+				<div class="one-half">
+					<a href="<?php echo $value['_pgsa_photos_after_photo']; ?>" data-lity><img itemprop="image"  class="aligncenter imageresource" src="<?php echo $value['_pgsa_photos_after_photo']; ?>" /></a>
+					<p class="pgsa-photo-caption"><span class="pgsa-photo-caption pgsa-photo-caption-after">After</span><?php if (!empty($value['_pgsa_photos_after_photo'])) { ?> <?php echo $value['_pgsa_photos_after_caption']; ?><?php } ?></p>
+				</div>
+			</div>
+		<?php } ?>
+		</section>
 	</article>
 
 	<?php endwhile; else : ?>
