8b388bfa8188a4b3d0253c26f8fecdc2b3834649
Angel Yarmas <angelmoskera@gmail.com>
2015-09-21 09:26:47 -0500

    Photo gallery template now loop around the taxonomy "Procedures"

--- templates/archive-photo-gallery.php
+++ templates/archive-photo-gallery.php
@@ -4,24 +4,40 @@
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 add_action( 'genesis_loop', 'pgsa_custom_archive_loop' );
 
-function pgsa_custom_archive_loop() { ?>
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <?php
-    $patient_photos = get_post_meta( get_the_ID(), '_pgsa_photos_patient-photos', true );
-		    $age 	= get_post_meta( get_the_ID(), '_pgsa_info_age', true );
-		    $gender = get_post_meta( get_the_ID(), '_pgsa_info_gender_select', true );
-		    $ethnic = get_post_meta( get_the_ID(), '_pgsa_info_ethnicity_select', true );
-		    $height = get_post_meta( get_the_ID(), '_pgsa_info_height', true );
-		    $weight = get_post_meta( get_the_ID(), '_pgsa_info_weight', true );
-	    	$detail = get_post_meta( get_the_ID(), '_pgsa_info_procedure_detail', true );
-	// Echo the metadata
-	?>
+function pgsa_custom_archive_loop() { 
 
-	<article <?php post_class() ?> >
+//Loop Variables
+$terms = get_terms("procedures");
+$count = count($terms);
 
+if ( $count > 0 ){
+    foreach ( $terms as $term ) {
+    	$term_link = get_term_link( $term );
+        echo '<h2><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></h2>';
+        $loop = new WP_Query( array( 
+            'post_type' => 'photo-gallery',
+            'post_per_page' => -1,
+            'showposts' => 1,
+            'orderby' => 'date',
+            'order' => 'ASC',
+            'tax_query' => array(
+                array(
+                    'taxonomy' => 'procedures',
+                    'field' => 'id',
+                    'terms' => $term->term_id
+                )
+            )
+        ));
+        // the loop
+        while ($loop->have_posts()) : $loop->the_post();
+            // do loop content
+        ?>
+		<?php global $post;
+		$patient_photos = get_post_meta( get_the_ID(), '_pgsa_photos_patient-photos', true );
+		 ?>
 		<?php foreach ( $patient_photos as $index => $value ) { ?>
-			<?php if ($index == 0) { ?> <!-- Show only first set of photos -->
-			<div class="one-fourth first">							
+			<?php if ($index <= 1) { ?> <!-- Show only the first TWO set of photos -->
+			<div class="one-fourth ">							
 				<a href="<?php the_permalink() ?>"><img src="<?php echo $value['_pgsa_photos_before_photo']; ?>" /></a>
 			</div>
 			<div class="one-fourth">								
@@ -30,39 +46,20 @@ function pgsa_custom_archive_loop() { ?>
 			<?php } ?>
 		<?php } ?>
 
-		<div class="two-fourths pgsa-patient-info">
-			
- 			<h3><?php echo get_the_term_list( $post->ID, 'procedures', '', ', ', '' ); ?> </h3>
- 			<section class="one-half first">
- 			<ul>
- 				<li><span class="pgsa-info-label-archive pgsa-info-label-gender">Age:</span> <?php echo esc_html( $age ); ?></li>
- 				<li><span class="pgsa-info-label-archive pgsa-info-label-gender">Gender:</span> <?php echo esc_html( $gender ); ?></li>
- 				<li><span class="pgsa-info-label-archive pgsa-info-label-gender">Ethnicity:</span> <?php echo esc_html( $ethnic ); ?></li>
- 			</ul>
- 			</section>
- 			<section class="one-half">
- 			<ul>
- 				<li><span class="pgsa-info-label-archive pgsa-info-label-gender">Height:</span> <?php echo esc_html( $height ); ?></li>
- 				<li><span class="pgsa-info-label-archive pgsa-info-label-gender">Weight:</span> <?php echo esc_html( $weight ); ?> </li>
- 			</ul>
 
- 			<a href="<?php the_permalink() ?>">View Photos <i class="fa fa-angle-double-right"></i></a>
- 			</section>
- 			
-		</div>
-	</article>
-
-
-	<?php endwhile; else : ?>
-		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-	<?php endif; ?>
-
-<?php }
+		<?php 
+        endwhile;
+        // reset $post so that the rest of the template is in the original context
+        wp_reset_postdata();
+        echo '<div class="clearfix"></div>';
+    }
+}
+}
 
 // Add Custom Scripts & CSS
-add_action( 'wp_enqueue_scripts', 'pgsa_script_css_single' );
-function pgsa_script_css_single() {
-	wp_enqueue_style( 'photo_gallery_single_styles', plugin_dir_url( __FILE__ ) . 'css/archive-styles.css' );
+add_action( 'wp_enqueue_scripts', 'pgsa_script_css_archive' );
+function pgsa_script_css_archive() {
+	wp_enqueue_style( 'photo_gallery_archive_styles', plugin_dir_url( __FILE__ ) . 'css/archive-styles.css' );
 }
 
 genesis();
