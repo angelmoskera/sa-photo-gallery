07f385915796bfb7f85a379e3603527405a6e112
Angel Yarmas <angelmoskera@gmail.com>
2015-09-21 14:51:16 -0500

    Fix issue with post classes not appearing correctly

--- templates/archive-photo-gallery.php
+++ templates/archive-photo-gallery.php
@@ -12,8 +12,14 @@ $count = count($terms);
 
 if ( $count > 0 ){
     foreach ( $terms as $term ) {
+
     	$term_link = get_term_link( $term );
-        echo '<h2><a href="' . esc_url( $term_link ) . '">' . $term->name . '</a></h2>';
+    	 
+    	?>
+
+        
+
+		<?php
         $loop = new WP_Query( array( 
             'post_type' => 'photo-gallery',
             'post_per_page' => -1,
@@ -28,30 +34,39 @@ if ( $count > 0 ){
                 )
             )
         ));
+
         // the loop
         while ($loop->have_posts()) : $loop->the_post();
-            // do loop content
+        $patient_photos = get_post_meta( get_the_ID(), '_pgsa_photos_patient-photos', true );
         ?>
-		<?php global $post;
-		$patient_photos = get_post_meta( get_the_ID(), '_pgsa_photos_patient-photos', true );
-		 ?>
+        <article <?php post_class() ?>>
+<h2><a href="<?php esc_url( $term_link ) ?>"><?php echo $term->name ?></a></h2>
+
 		<?php foreach ( $patient_photos as $index => $value ) { ?>
 			<?php if ($index <= 1) { ?> <!-- Show only the first TWO set of photos -->
-			<div class="one-fourth ">							
+			
+			<div class="one-fourth">		 					
 				<a href="<?php the_permalink() ?>"><img src="<?php echo $value['_pgsa_photos_before_photo']; ?>" /></a>
 			</div>
 			<div class="one-fourth">								
 				<a href="<?php the_permalink() ?>"><img src="<?php echo $value['_pgsa_photos_after_photo']; ?>" /></a>
 			</div>
+			
 			<?php } ?>
 		<?php } ?>
+		</article>
+
+
+		
 
 
 		<?php 
         endwhile;
         // reset $post so that the rest of the template is in the original context
         wp_reset_postdata();
-        echo '<div class="clearfix"></div>';
+        ?>
+ 
+        <?php
     }
 }
 }
